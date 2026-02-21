5fc4c3238e9a2b5b428ba39789c6718fd368799d
raul-profesor <r.riesco@edu.gva.es>
2026-02-21 16:45:00 +0100

    creación bbdd libros

--- mySqlDB/bookDB.sql
+++ mySqlDB/bookDB.sql
@@ -15,6 +15,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE DATABASE IF NOT EXISTS `bookstore`;
+USE `bookstore`;
+
 --
 -- Table structure for table `book_category_relationship`
 --
