6d8ee69c2436b41374d439f8479c633e8dff4eb2
alexvc1999 <alejandro99vicente@gmail.com>
2026-02-23 18:13:27 +0100

    cambios en dev 2

--- www/html/index.php
+++ www/html/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Movie DataBase</title>
     <meta charset="utf-8">
@@ -14,50 +15,47 @@
 
 <body>
 
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">My Website</a>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">My Website</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="./books/home.php">Books</a></li>
+                <li><a href="./movies/home.php">Movies</a></li>
+            </ul>
         </div>
-        <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="./books/home.php">Books</a></li>
-            <li><a href="./movies/home.php">Movies</a></li>
-        </ul>
+    </nav>
+
+    <div class="container">
+        <h2>What should I do??</h2>
+        <div class="well well-lg">Thinking...</div>
+
+        <div class="row">
+            <div class="col-md-6">
+
+                <a href="./books/home.php">
+                    <img src="./siteImages/books.jpg" class="img-circle" alt="books" width="400" height="400">
+                    <div class="caption">
+                        <p>Study Time...</p>
+                    </div>
+                </a>
+
+            </div>
+            <div class="col-md-6">
+
+                <a href="./movies/home.php">
+                    <img src="./siteImages/movies.jpg" class="img-circle" alt="movies" width="400" height="400">
+                    <div class="caption">
+                        <p>Movie Time...</p>
+                    </div>
+                </a>
+
+            </div>
+        </div>
     </div>
-</nav>
-
-<div class="container">
-    <h2>What should I do??</h2>
-    <div class="well well-lg">Thinking...</div>
-
-    <div class="row">
-        <div class="col-md-6">
-
-            <a href="./books/home.php">
-                <img src="./siteImages/books.jpg" class="img-circle" alt="books" width="400" height="400">
-                <div class="caption">
-                    <p>Study Time...</p>
-                </div>
-            </a>
-
-        </div>
-        <div class="col-md-6">
-
-            <a href="./movies/home.php">
-                <img src="./siteImages/movies.jpg" class="img-circle" alt="movies" width="400" height="400">
-                <div class="caption">
-                    <p>Movie Time...</p>
-                </div>
-            </a>
-
-        </div>
-    </div>
-</div>
 
 </body>
+
 </html>
-
-
-
-
